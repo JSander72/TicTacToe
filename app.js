9038784d368b97e2c44a16b3d5ec33162eaa8fc9
JSander72 <jamesearlsanders2@outlook.com>
2023-03-08 13:33:26 -0500

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+let cells = document.querySelectorAll(".row > div");
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", cellClicked);
+}
+
+
+function cellClicked () {
+  Event.target.textContent = "X"
+}
+//if else statments are your friends!
+cells[2].textContent
+
+
